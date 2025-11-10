c20e97348b4f2b823893b9ab70a6e99e01ac22f1
kiralightyagami <crucisgamer98@gmail.com>
2025-11-10 12:53:45 +0530

    node ui using react-flow

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,8 +2,22 @@
 
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
 import { ErrorView, LoadingView } from "@/components/entity-views";
-
-
+import { useState, useCallback } from 'react';
+import { 
+    ReactFlow, 
+    applyNodeChanges, 
+    applyEdgeChanges, 
+    addEdge, 
+    type Node,
+    type Edge,
+    NodeChange,
+    EdgeChange,
+    Connection,
+    Background,
+    Controls,
+    MiniMap
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
 
 export const EditorLoading = () => {
     return <LoadingView message="Loading workflow..." />;
@@ -13,11 +27,59 @@ export const EditorError = () => {
     return <ErrorView message="Error loading workflow" />;
 }
 
+const initialNodes = [
+    { 
+        id: 'n1', 
+        position: { x: 0, y: 0 }, 
+        data: { label: 'Node 1' } 
+    },
+    { 
+        id: 'n2', 
+        position: { x: 0, y: 100 }, 
+        data: { label: 'Node 2' } 
+    },
+  ];
+
+  const initialEdges = [
+    { 
+        id: 'n1-n2', 
+        source: 'n1', 
+        target: 'n2' 
+    }];  
+
 export const Editor = ({ workflowId }: { workflowId: string }) => {
     const { data: workflow } = useSuspenseWorkflow(workflowId);
+    const [nodes, setNodes] = useState<Node[]>(initialNodes);
+    const [edges, setEdges] = useState<Edge[]>(initialEdges);
+    
+    const onNodesChange = useCallback(
+        (changes: NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+        [],
+      );
+      const onEdgesChange = useCallback(
+        (changes: EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+        [],
+      );
+      const onConnect = useCallback(
+        (params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+        [],
+      )    
+    
     return (
-        <p>
-            {JSON.stringify(workflow, null, 2)}
-        </p>
+        <div className="size-full">
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView
+            >
+                <Background />
+                <Controls />
+                <MiniMap />
+                
+            </ReactFlow>
+        </div>
     );
 };
