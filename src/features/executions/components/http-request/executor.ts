5b32b0431471a0d25c507fdb400f4228e0380fa9
kiralightyagami <crucisgamer98@gmail.com>
2025-11-12 17:31:43 +0530

    fix: key collision

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -3,6 +3,7 @@ import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions} from "ky";
 
 type HttpRequestData = {
+  variableName?: string;
   endpoint?: string;
   method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
   body?: string;
@@ -17,6 +18,10 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     
   if (!data.endpoint) {
     throw new NonRetriableError("Endpoint is required");
+  }
+
+  if (!data.variableName) {
+    throw new NonRetriableError("Variable name is required");
   }
 
   const result = await step.run("http-request", async () => {
@@ -38,17 +43,23 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     const contentType = response.headers.get("content-type");
     const responseData = contentType?.includes("application/json") ? await response.json() : await response.text();
 
-    return{
-      ...context,
+    const responsePayload = {
       httpResponse: {
         status: response.status,
         statusText: response.statusText,
         data: responseData,
 
       },
+    };
+    
+
+    if (data.variableName) {
+      return {
+        ...context,
+        [data.variableName]: responsePayload,
+      };
     }
-
-  });
-
-  return result;
+    return { ...context, ...responsePayload };
+});
+   return result;
 };
