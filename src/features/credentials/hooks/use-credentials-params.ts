5d116d7f3b0373ba1149d084e236cd06546b2ccf
kiralightyagami <crucisgamer98@gmail.com>
2025-11-13 19:42:30 +0530

    added hooks for credentials

---
+++ src/features/credentials/hooks/use-credentials-params.ts
@@ -0,0 +1,8 @@
+import { useQueryStates } from "nuqs";
+import { credentialsParams } from "../params";
+
+export const useCredentialsParams = () => {
+    return useQueryStates(credentialsParams);
+};
+
+
